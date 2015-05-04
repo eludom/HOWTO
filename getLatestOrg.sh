236ab60afe1f3dd0d645a3a82ebdf8b905e60afe
George Jones <gmj@pobox.com>
2015-05-04 07:37:12 -0400

    Move location to ~/git/orgmode.org/

--- getLatestOrg.sh
+++ getLatestOrg.sh
@@ -1,30 +1,32 @@
 #! /bin/sh -x -u -e
 #
-# Get latest version of org-mode in ~/elisp/org-mode
+# Get latest version of org-mode in ~/git/orgmode.org/org-mode
 #
 
-mkdir -p ~/elisp
-cd ~/elisp
+WHERE="${HOME}/git/orgmode.org/"
+mkdir -p ${WHERE}
+cd ${WHERE}
 
 # pull for first time if we don't have it
 
-if [ ! -d ~/elisp/org-mode ]; then
+if [ ! -d ${WHERE}/org-mode ]; then
     git clone git://orgmode.org/org-mode.git
 else
 
     # get updates, if any
     
-    cd ~/elisp/org-mode
+    cd ${WHERE}/org-mode
     git pull
 fi
 
 # build the autoloads
-cd ~/elisp/org-mode
+cd ${WHERE}/org-mode
+make
 make autoloads
 
 # Add to .emacs, .emacs.d/init.el, whatever...
 #
-#(add-to-list 'load-path "~/elisp/org-mode/lisp/")
-#(add-to-list 'load-path "~/elisp/org-mode/contrib/lisp/" t)
+#(add-to-list 'load-path "~/git/orgmode.org/org-mode/lisp/")
+#(add-to-list 'load-path "~/elisp/orgmodeorg/org-mode/contrib/lisp/" t)
 
    
